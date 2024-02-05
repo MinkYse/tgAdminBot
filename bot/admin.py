5531f41b3d1d3ced239c8f5b9eb7546469643ef7
ulia <sete855@gmail.com>
2024-02-05 20:14:44 +0300

    Change model and add admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import (
+    Client,
+    Seller,
+    Service,
+    Hotel
+)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('username', 'tg_id', 'register_at')
+    search_fields = ('username', 'tg_id',)
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'tg_id', 'register_at')
+    search_fields = ('username', 'tg_id',)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'region', 'description', 'phone_number', 'price', 'category')
+    search_fields = ('name', 'address', 'region', 'phone_number', 'category',)
+    list_filter = ('region', 'category')
+
+    @admin.display(description='Диапазон цен')
+    def price(self, obj):
+        return f'От {obj.min_price}₽ до {obj.max_price}₽'
+    price.short_description = 'Диапазон цен'
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'region', 'description', 'phone_number', 'price', 'category')
+    search_fields = ('name', 'address', 'region', 'phone_number', 'category',)
+    list_filter = ('region', 'category')
+
+    @admin.display(description='Диапазон цен')
+    def price(self, obj):
+        return f'От {obj.min_price}₽ до {obj.max_price}₽'
+    price.short_description = 'Диапазон цен'
+    
--- bot/models.py
+++ bot/models.py
@@ -2,54 +2,76 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-class Client(models.Model):
-    username = models.CharField(unique=True, db_index=True)
-    tg_id = models.IntegerField(unique=True)
-    register_at = models.DateField(auto_now_add=True)
+class UserBase(models.Model):
+    username = models.CharField(unique=True, db_index=True, verbose_name='Имя пользователя')
+    tg_id = models.IntegerField(unique=True, verbose_name='ID Telegram')
+    register_at = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.username
+
+
+class Client(UserBase):
 
     class Meta:
         ordering = ('-created',)
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
 
-    def __str__(self):
-        return self.username
 
-
-class Seller(models.Model):
-    username = models.CharField(unique=True, db_index=True)
-    tg_id = models.IntegerField(unique=True)
-    register_at = models.DateField(auto_now_add=True)
+class Seller(UserBase):
 
     class Meta:
         ordering = ('-created',)
         verbose_name = 'Продавец'
         verbose_name_plural = 'Продавцы'
 
-    def __str__(self):
-        return self.username
-
 
 class Category(models.Model):
     name = models.CharField(unique=True)
     type = models.CharField(choices=('Услуги', 'Отели'))
 
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
-class Hotel(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    phone_number = models.CharField(unique=True, max_length=12)
-    max_price = models.PositiveIntegerField()
-    min_price = models.PositiveIntegerField()
-    image = ArrayField(models.CharField(), size=3)
-    category = models.ForeignKey(Category, related_name='hotels', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 
-class Service(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    phone_number = models.CharField(unique=True, max_length=12)
-    max_price = models.PositiveIntegerField()
-    min_price = models.PositiveIntegerField()
-    image = ArrayField(models.CharField(), size=3)
-    category = models.ForeignKey(Category, related_name='hotels', on_delete=models.CASCADE)
+class ServiceBase(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+    region = models.CharField(max_length=255, verbose_name='Район')
+    description = models.TextField(verbose_name='Описание')
+    phone_number = models.CharField(unique=True, max_length=12, verbose_name='Номер телефона')
+    max_price = models.PositiveIntegerField(verbose_name='Максимальная цена')
+    min_price = models.PositiveIntegerField(verbose_name='Минимальная цена')
+    image = ArrayField(models.CharField(), size=3, verbose_name='Фотографии')
+    category = models.ForeignKey(Category, related_name='hotels', on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Hotel(ServiceBase):
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Отель'
+        verbose_name_plural = 'Отели'
+
+
+class Service(ServiceBase):
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
